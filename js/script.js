ee1cf78d32fce68de706be6ff4c9ed0a0fafeb84
lucasvittor <dev.lucssvitor@gmail.com>
2025-12-12 11:23:17 -0300

    Add - Liturgia Diaria with N8N

--- js/script.js
+++ js/script.js
@@ -37,6 +37,53 @@ links.forEach(link => {
 
 window.addEventListener('scroll', () => {
   revealOnScroll();
-  animateSkills();
 });
 
+// Input Liturgia Diaria
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById('subscribeForm');
+    const feedback = document.getElementById('form-feedback');
+    const btn = document.getElementById('btn-submit');
+
+    form.addEventListener('submit', async function(e) {
+        e.preventDefault();
+        
+        // Carregando
+        const originalText = btn.innerHTML;
+        btn.innerHTML = 'Enviando...';
+        btn.disabled = true;
+
+        const nome = document.getElementById('nome').value;
+        const email = document.getElementById('email').value;
+
+        // Dados
+        const data = { nome: nome, email: email };
+
+        try {
+            
+            const webhookURL = 'https://iastation.app.n8n.cloud/webhook-test/32c7fc5c-26f7-4dac-bb2d-6ec2f4c834e9'; 
+
+            await fetch(webhookURL, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data)
+            });
+
+            feedback.style.display = 'block';
+            feedback.innerText = 'Inscrição realizada com sucesso! Verifique seu e-mail em breve.';
+            feedback.style.color = '#4caf50';
+            form.reset();
+
+        } catch (error) {
+            console.error('Erro:', error);
+            feedback.style.display = 'block';
+            feedback.innerText = 'Houve um erro. Tente novamente mais tarde.';
+            feedback.style.color = '#ff5252';
+        } finally {
+            btn.innerHTML = originalText;
+            btn.disabled = false;
+        }
+    });
+});
